c587fa46020170630231701249a17175b32a8a79
Yahyaev Danis <67186974+Danisssssss@users.noreply.github.com>
2023-09-19 20:00:46 +0400

    Update Laba 2.cpp

--- ASM/Laba 2/Laba 2.cpp
+++ ASM/Laba 2/Laba 2.cpp
@@ -8,6 +8,13 @@ int main() {
     cout << "Номер практического занятия: Лабораторная работа №3-4" << endl;
     cout << "Номер варианта: №18 Вычислить (a*c - a*b) / (b^2 - c^2) + (b^2 + a*b) / (3*a^2 - 3*a*b)" << endl;
     cout << "=========================" << endl;
+    // Вычислить: Y = d - b - a + c
+    // -- Тестовые значения-- 
+    // для a = 2, b = 4, c = 3 ожидается ответ -2
+    // для a = 3, b = 5, c = 4 ожидается ответ -2
+    // для a = 7, b = 4, c = 5 ожидается ответ 0
+    // для a = 2, b = 5, c = 1 ожидается ответ -1
+    // для a = -4, b = -5, c = -2 ожидается ответ -3
 
     int chisl1 = 0;
     int zn1 = 0;
@@ -74,30 +81,21 @@ int main() {
         mov eax, chisl1;
         cdq; // Расширяем eax до edx:eax для деления
         idiv zn1; // Делим числитель первой дроби на знаменатель
+        mov ebx, eax;
 
-        mov ebx, chisl2; //
+        mov eax, chisl2; //
         cdq;
         idiv zn2;
         add eax, ebx;
         mov resa, eax;
     }
-    /*
-    int chisl1 = 0;
-    int zn1 = 0;
-    int chisl2 = 0;
-    int zn2 = 0;
-    int resa = 0;
-    int resc = 0;
-
-    int a2, b2, c2, ac, ab;
-    */
 
     cout << "Проверка на Assembler:" << endl;
-    cout << "chisl1 = " << chisl1 << endl;
-    cout << "zn1 = " << zn1 << endl;
-    cout << "chisl2 = " << chisl2 << endl;
-    cout << "zn2 = " << zn2 << endl;
-    cout << "результат: " << resa << "\n\n";
+    cout << "Числитель первой дроби chisl1 = " << chisl1 << endl;
+    cout << "Знаменатель первой дроби zn1 = " << zn1 << endl;
+    cout << "Числитель второй дроби chisl2 = " << chisl2 << endl;
+    cout << "Знаменатель второй дроби zn2 = " << zn2 << endl;
+    cout << "Результат res = " << resa << "\n\n";
 
     resc = (a * c - a * b) / (b * b - c * c) + (b * b + a * b) / (3 * a * a - 3 * a * b);
     cout << "Проверка на C++:" << endl;
@@ -105,11 +103,12 @@ int main() {
     chisl2 = b * b + a * b;
     zn1 = b * b - c * c;
     zn2 = 3 * a * a - 3 * a * b;
-    cout << "chisl1 = " << chisl1 << endl;
-    cout << "zn1 = " << zn1 << endl;
-    cout << "chisl2 = " << chisl2 << endl;
-    cout << "zn2 = " << zn2 << endl;
-    cout << "результат: " << resc << endl;
+    cout << "Проверка на C++:" << endl;
+    cout << "Числитель первой дроби chisl1 = " << chisl1 << endl;
+    cout << "Знаменатель первой дроби zn1 = " << zn1 << endl;
+    cout << "Числитель второй дроби chisl2 = " << chisl2 << endl;
+    cout << "Знаменатель второй дроби zn2 = " << zn2 << endl;
+    cout << "Результат res = " << resa << "\n\n";
     cout << "(" << a << "*" << c << "-" << a << "*" << b << ")/(" << b << "^2-" << c << "^2) + (" << b << "^2+" << a << "*" << b << ")/( 3*" << a << "^2-3" << "*" << a << "*" << b << ")=" << resc << endl;
 
     return 0;
