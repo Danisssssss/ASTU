412b71aa974d922bd5f57a92335e1b439e6028e9
Yahyaev Danis <67186974+Danisssssss@users.noreply.github.com>
2023-09-19 19:37:11 +0400

    Update Laba 2.cpp

--- ASM/Laba 2/Laba 2.cpp
+++ ASM/Laba 2/Laba 2.cpp
@@ -1,94 +1,116 @@
-#include <iostream> // подключаем библиотеку с функциями ввода-вывода (printf, system)
-using namespace std; // используем пространство имен std
+#include <iostream>
+using namespace std;
 
-int main() { // обычный код на C++
-    setlocale(LC_ALL, "RU"); // Подключение русского языка
-    // Вывод информации о разработчике и номере варианта
+int main() {
+    setlocale(LC_ALL, "RU");
+
     cout << "Фамилия разработчика: Яхьяев Данис" << endl;
     cout << "Номер практического занятия: Лабораторная работа №3-4" << endl;
     cout << "Номер варианта: №18 Вычислить (a*c - a*b) / (b^2 - c^2) + (b^2 + a*b) / (3*a^2 - 3*a*b)" << endl;
     cout << "=========================" << endl;
-    // Вычислить: (ac - ab) / (b^2 - c^2) + (b^2 + ab) / (3a^2 - 3ab)
-    // -- Тестовые значения-- 
-    // для a = , b = , c = , ожидается ответ
-    // для a = , b = , c = , ожидается ответ
-    // для a = , b = , c = , ожидается ответ
 
-    int chisl1 = 0; // переменная для хранения первого числителя дроби, равного ac - ab
-    int zn1 = 0; // переменная для хранения первого знаменателя дроби, равного b^2 - c^2
-    int chisl2 = 0; // переменная для хранения второго числителя дроби, равного b^2 + ab
-    int zn2 = 0; // переменная для хранения второго знаменателя дроби, равного 3a^2 - 3ab
-    int resa = 0; // результат вычисления выражения на Ассемблере
-    int resc = 0; // результат вычисления выражения на C++
+    int chisl1 = 0;
+    int zn1 = 0;
+    int chisl2 = 0;
+    int zn2 = 0;
+    int resa = 0;
+    int resc = 0;
 
-    int a2,  // переменная для хранения a^2
-        b2, // переменная для хранения b^2
-        c2, // переменная для хранения c^2
-        ac, // переменная для хранения a*c
-        ab; // переменная для хранения a*b
-
-    int a, b, c; // Объявление переменных для хранения значений a, b и c
-    cout << "Введите значения a, b и c: "; // Вывод сообщения на экран и ожидание ввода значений a, b и c
+    int a2, b2, c2, ac, ab;
+    int a, b, c;
+    cout << "Введите значения a, b и c: ";
     cin >> a >> b >> c;
-    
-    // Вставка ассемблерного кода для вычисления Y
+
+    // Расчет необходимых промежуточных значений
     __asm {
-        // Вычисление a^2, b^2 и c^2
-        mov eax, a
-        imul eax, a
-        mov a2, eax
-    
-        mov ebx, b
-        imul ebx, b
-        mov b2, ebx
-    
-        mov ecx, c
-        imul ecx, c
-        mov c2, ecx
-    
-        // Вычисление a*c и a*b
-        mov eax, a
-        imul eax, c
-        mov ac, eax
-    
-        mov ebx, a
-        imul ebx, b
-        mov ab, ebx
-    
-        // Вычисление числителя и знаменателя первой дроби: (ac - ab) / (b^2 - c^2)
-        sub eax, ebx
-        mov chisl1, eax
-    
-        sub b2, c2
-        mov zn1, b2
-    
-        // Вычисление числителя и знаменателя второй дроби: (b^2 + ab) / (3a^2 - 3ab)
-        add b2, ebx
-        mov chisl2, b2
-    
-        mov edx, 3
-        imul edx, a2
-    
-        mov ebx, 3
-        imul ebx, ab
-    
-        sub edx, ebx
-        mov zn2, edx
-    
-        // Вычисление итогового результата
-        mov eax, chisl1
-        imul eax, zn2
-    
-        mov ebx, chisl2
-        imul ebx, zn1
-    
-        add eax, ebx
-        mov resa, eax
+        // a^2
+        mov eax, a; // a
+        imul eax, eax; // a ^ 2
+        mov a2, eax; // 
+
+        // b^2
+        mov eax, b; // b
+        imul eax, eax; // b ^ 2
+        mov b2, eax; // 
+
+        // c^2
+        mov eax, c; // c
+        imul eax, eax; // c ^ 2
+        mov c2, eax; // 
+
+        // ac
+        mov eax, a; // a
+        imul eax, c; // a * c
+        mov ac, eax; // 
+
+        mov eax, a; // a
+        imul eax, b; // a * b
+        mov ab, eax; // 
+
+        // Вычисление числителя первой дроби
+        mov eax, ac; // ac
+        sub eax, ab; // ac - ab (числитель)
+        mov chisl1, eax; //
+
+        // Вычисление знаменателя первой дроби
+        mov eax, b2; //  b^2
+        sub eax, c2; // b ^ 2 - c ^ 2 (знаменатель)
+        mov zn1, eax; // 
+
+        // Вычисление числителя второй дроби
+        mov eax, b2; // b^2
+        add eax, ab; // b ^ 2 + ab(числитель)
+        mov chisl2, eax; // 
+
+        // Вычисление знаменателя второй дроби
+        mov eax, a2; // a^2
+        imul eax, 3; // 3*a^2
+        mov ebx, ab; // ab
+        imul ebx, 3; // 3 * ab
+        sub eax, ebx; // 3*a^2 - 3*ab
+        mov zn2, eax; // 
+
+        // Вычисление результата
+        mov eax, chisl1;
+        cdq; // Расширяем eax до edx:eax для деления
+        idiv zn1; // Делим числитель первой дроби на знаменатель
+
+        mov ebx, chisl2; //
+        cdq;
+        idiv zn2;
+        add eax, ebx;
+        mov resa, eax;
     }
+    /*
+    int chisl1 = 0;
+    int zn1 = 0;
+    int chisl2 = 0;
+    int zn2 = 0;
+    int resa = 0;
+    int resc = 0;
 
-    // Вывод результата на экран
-    cout << "Результат на ассемблере: " << resa << endl;
+    int a2, b2, c2, ac, ab;
+    */
 
+    cout << "Проверка на Assembler:" << endl;
+    cout << "chisl1 = " << chisl1 << endl;
+    cout << "zn1 = " << zn1 << endl;
+    cout << "chisl2 = " << chisl2 << endl;
+    cout << "zn2 = " << zn2 << endl;
+    cout << "результат: " << resa << "\n\n";
 
-    return 0; // Завершение программы и возвращение нулевого значения
+    resc = (a * c - a * b) / (b * b - c * c) + (b * b + a * b) / (3 * a * a - 3 * a * b);
+    cout << "Проверка на C++:" << endl;
+    chisl1 = a * c - a * b;
+    chisl2 = b * b + a * b;
+    zn1 = b * b - c * c;
+    zn2 = 3 * a * a - 3 * a * b;
+    cout << "chisl1 = " << chisl1 << endl;
+    cout << "zn1 = " << zn1 << endl;
+    cout << "chisl2 = " << chisl2 << endl;
+    cout << "zn2 = " << zn2 << endl;
+    cout << "результат: " << resc << endl;
+    cout << "(" << a << "*" << c << "-" << a << "*" << b << ")/(" << b << "^2-" << c << "^2) + (" << b << "^2+" << a << "*" << b << ")/( 3*" << a << "^2-3" << "*" << a << "*" << b << ")=" << resc << endl;
+
+    return 0;
 }
