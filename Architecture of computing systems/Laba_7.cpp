a6062f79ed8519617e4eb4cf7bfe08b58d74f38f
Yahyaev Danis <67186974+Danisssssss@users.noreply.github.com>
2023-11-02 17:53:52 +0400

    Add files via upload

--- Architecture of computing systems/Laba_7.cpp
+++ Architecture of computing systems/Laba_7.cpp
@@ -5,26 +5,23 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "RU");
-    float zero = 0;
-    float x1, x2, y1, x3, x4, y2, y3, y4;
     float s;
-    // cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
-    // 2 4 12 0 10 6 14 0
-    x1 = 2; y1 = 4; x2 = 12; y2 = 0; x3 = 10; y3 = 6; x4 = 14; y4 = 0;
+    float zero = 0;
+    float y2 = 0, y4 = 0;
+    float x1, y1, x2, x3, y3, x4;
+
+    cout << "Яхьяев Данис. Лабораторная работа № 13-14. Вариант №18." << endl;
+
+    // -- Тестовые данные --
+    // x1 = 2 y1 = 4 x2 = 12 x3 = 10 y3 = 6 x4 = 14, то s = 8
+    // x1 = 2 y1 = 4 x2 = 12 x3 = 11 y3 = 6 x4 = 14, то s = 4
+    // x1 = 3 y1 = 5 x2 = 10 x3 = 8 y3 = 2 x4 = 14, то s = 4
+    // x1 = 6 y1 = 2 x2 = 4 x3 = 18 y3 = 12 x4 = 10, то s = 4
+    // x1 = 4 y1 = 14 x2 = 16 x3 = 2 y3 = 19 x4 = 20, то s = 168
+    cout << "Введите х1, у1, х2, х3, у3, х4: ";
+    cin >> x1 >> y1 >> x2 >> x3 >> y3 >> x4;
 
     __asm {
-        // Сравнение x2 и x3
-        fld x2 // Загрузка значения x2 в st(0)
-        fld x3 // Загрузка значения x3 в st(0), st(1) = x2
-        fcomi st, st(1)
-        jae zero_area // если x3 >= x2, то переходим на метку zero_area
-
-        // Сравнение x4 и x1
-        fld x4 // Загрузка значения x4 в st(0)
-        fld x1 // Загрузка значения x1 в st(0), st(1) = x4
-        fcomi st, st(1) // сравнение
-        jae zero_area // если x1 >= x4, то переходим на метку zero_area
-
         // Сравнение y1 и y3
         fld y3 // Загрузка значения y3 в st(0)
         fld y1 // Загрузка значения y1 в st(0), st(1) = y3
@@ -46,6 +43,7 @@ int main() {
             fcomi st, st(1) // сравнение
 
             jae y1_less_y3_x3_more_x1_and_x4_more_x2 // если x4 >= x2, то переходим на метку y1_less_y3_x3_more_x1_and_x4_more_x2
+            jb y1_less_y3_x3_more_x1_and_x4_less_x2 // если x4 <= x2, то переходим на метку y1_less_y3_x3_more_x1_and_x4_less_x2
             
             y1_less_y3_x3_more_x1_and_x4_more_x2 :
         fld x2 // Загрузка значения x2 в st(0)
@@ -54,8 +52,6 @@ int main() {
             fld y1 // st(0) = y1, st(1) = abs(x2-x3)
             fmul // abs(x2-x3) * y1
             jmp finish // переход к финишу
-
-            jbe y1_less_y3_x3_more_x1_and_x4_less_x2 // если x4 <= x2, то переходим на метку y1_less_y3_x3_more_x1_and_x4_less_x2
 
             y1_less_y3_x3_more_x1_and_x4_less_x2 :
         fld x4 // Загрузка значения x4 в st(0)
@@ -85,7 +81,6 @@ int main() {
             fabs // st(0) = abs(x2-x1)
             fmul y1 // st(0) = abs(x2-x1) * y1
             jmp finish // переход к финишу
-
 
         y1_more_y3 :
         fld x1 // Загрузка значения x1 в st(0)
@@ -166,7 +161,8 @@ int main() {
                 s = abs((x4 - x1)) * y1;
         }
     }
-    cout << "Проверка на C++: " << s;
+    cout << "Проверка на C++: " << s << endl;
 
+    system("pause");
     return 0;
 }
